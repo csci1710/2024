0d03818d6e11f294e26eeb14a5e135970beea8df
Tim Nelson <tn@cs.brown.edu>
2023-02-03 12:18:51 -0500

    feat: video link and livecode feb 03

--- my-app/src/SITE_DATA.ts
+++ my-app/src/SITE_DATA.ts
@@ -88,14 +88,19 @@ export const lectureWeeks: LectureWeek[] = [
         name: "Intro to Modeling Systems (3): Traces, Examples",
         date: "Feb 03",
         notesLink: "https://hackmd.io/@lfs/rkXCRYq2i",
-        // recordingLink: "https://www.google.com",
+        recordingLink: 
+          "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=454dc37b-ac45-47f1-a208-af850163de3c",
         liveCodeLink: process.env.PUBLIC_URL + "/livecode/feb03_ttt.frg",
-        // otherLinks: [
-        //   {
-        //     name: "OtherLink",
-        //     link: "https://www.google.com",
-        //   },
-        // ],
+        otherLinks: [
+          {
+            name: "Viz Script",
+            link: process.env.PUBLIC_URL + "/livecode/feb03_ttt.js",
+          },
+          {
+            name: "Tests",
+            link: process.env.PUBLIC_URL + "/livecode/feb03_ttt.tests.frg",
+          },
+        ],
       },
     ],
   },

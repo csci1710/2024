73dcc5177da4578a0b13767ddef6f7a35ec7d853
Tim Nelson <tbnelson@gmail.com>
2024-01-28 14:38:05 -0500

    preamble

--- my-app/src/components/LecturesTable.tsx
+++ my-app/src/components/LecturesTable.tsx
@@ -19,7 +19,7 @@ export default function LecturesTable(props: LectureTableProps) {
   return (
     <div className="mt-8 flex flex-col">
       <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <p>The listed lecture schedule is prospective and subject to change.</p>
+      <p>The listed lecture schedule is prospective and subject to change. All lecture capture videos can be found on our 2024 Canvas <a href="https://canvas.brown.edu/courses/1094402/external_tools/33943">media library page</a>. Notes can be found on our <a href="https://csci1710.github.io/book">Forge book deployment</a>, even if the specific chapters are not yet linked below.</p>
         <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
           <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
             <table className="min-w-full divide-y divide-gray-300">
